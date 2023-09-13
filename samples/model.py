13c863d94ac21231a4e5b95ec5355f309ae50aec
imamhossainroni <imamhossainroni95@gmail.com>
2023-09-14 01:00:32 +0600

    🚀 Meta class added to define class name

--- samples/model.py
+++ samples/model.py
@@ -3,7 +3,6 @@ from sqlkite.orm.models.fields import CharField, IntegerField
 
 
 class User(Model):
-    table_name = 'users'
 
     username = CharField(max_length=50)
     email = CharField(max_length=100)
